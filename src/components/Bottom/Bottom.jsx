46f7a9cb4794713e301d5de86fb4b95eeba29e7a
Tempesto20 <Bro74rus1@mail.ru>
2022-08-31 21:46:01 +0300

    add formik and yup

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -1,94 +1,74 @@
-import React,{useState} from 'react';
+//import React,{useState} from 'react';
 import '../../scss/components/bottom.scss';
+import { Formik } from 'formik';
 
 function Bottom() {
-    const[ mail, setMail ]=useState('');
-    
-    const validateEmail =(email)=> {
-		const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return re.test(email);
-	}
-    
-    const inputMail =<input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input"></input>;
-
-    const email = inputMail;
-
-    const mailTrue = <input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input ok"></input>;
-    const mailFalse = <input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input error"></input>;
-   
-    const chengeMail  = ()=>{
-
-        if(validateEmail(email.value)){
-            setMail(mailTrue);
-        } else{
-            setMail(mailFalse);
-        } 
-      }
-    
-
-
-/*    
-    const inputMail =<input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input"></input>;
-    const mailTrue = <input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input ok"></input>;
-    const mailFalse = <input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input error"></input>;
-	
-    const bottomButton =()=> {
-		const email = inputMail;
-		if (validateEmail(email.value)) {
-			email.classList.remove('error');
-			email.classList.add('ok');
-		} else {
-			email.classList.remove('ok');
-			email.classList.add('error');
-		}
-		return false;
-	}
-*/
 
 
   return (
     <div className="botton__content" id="news">
+        
     <div className="bottom__conteiner section-container">
         <div className="bottom__background">
             <div className="bottom__row">
                 <h2 className="bottom__title">Успей купить!</h2>
-                
-                
-                <form className="bottom_link">
-                    <div className="bottom__email">
-                        <div id="bottom__email-1">
+                            <Formik
+                                    initialValues={{ email: '', password: '' }}
+                                    validate={values => {
+                                        const errors = {};
+                                        if (!values.email) {
+                                        errors.email = 'Required';
+                                        } else if (
+                                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                                        ) {
+                                        errors.email = 'Invalid email address';
+                                        }
+                                        return errors;
+                                    }}
+                                    onSubmit={(values, { setSubmitting }) => {
+                                        setTimeout(() => {
+                                        alert(JSON.stringify(values, null, 2));
+                                        setSubmitting(false);
+                                        }, 400);
+                                    }}
+                                >
+                                    {({
+                                        values,
+                                        errors,
+                                        touched,
+                                        handleChange,
+                                        handleBlur,
+                                        handleSubmit,
+                                        isSubmitting,
+                                        /* and other goodies */
+                                    }) => (
+                                <form onSubmit={handleSubmit} className="bottom_link">
+                                    <div className="bottom__email">
+                                        <div id="bottom__email-1">
+                                                <input
+                                                    type="email"
+                                                    name="email"
+                                                    id="bottom__email-2" placeholder="Ваш e-mail" class="contacts-input"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.email}
+                                                />
+                                            </div>
+                                        </div>
 
-                        {inputMail !== (validateEmail(email.value)) ? (mailTrue):(mailFalse)}
-                        
-{/*                        
-                        { validateEmail(email.value) !== true ? 
-                        (<input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input ok"></input>) 
-                            :
-                        (<input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input error"></input>)
-                         }
-                         
-*/}
+                                    {errors.email && touched.email && errors.email}
+                                    
+                                    <div className="bottom__button">
+                                        <div className="button__batton">
+                                            <button type="submit" disabled={isSubmitting} className="bottom__button-text btn">
+                                                Подписаться
+                                            </button>
+                                        </div>
+                                    </div>
 
-
-{/*
-                        {mail && <input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input ok"></input>}
-                        {!mail &&<input id="bottom__email-2" type="text" placeholder="Ваш e-mail" className="contacts-input error"></input>}
-   */}                     
-                        {/*<input id="bottom__email-2" type="text" placeholder="Ваш e-mail" class="contacts-input"></input>*/}
-
-
-
-                        </div>
-                    </div>
-                    <div className="bottom__button">
-                        <div className="button__batton">
-                            <a href="#" className="bottom__button-text" onClick={chengeMail}>
-                                Подписаться
-                            </a>
-                        </div>
-                    </div>
-                </form>
-
+                                </form>
+                            )}
+                            </Formik>
 
             </div>
             <div className="bottom__row-1">
@@ -110,6 +90,14 @@ function Bottom() {
         </div>
     </div>
 </div>
+
+
+
+
+
+
+
+
   );
 }
 
